07f44450f73f4c96e785ea2c75be860c31afaf47
Arisa Sato <grow.up.anna@gmail.com>
2017-03-14 05:00:59 +0000

    20170314

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |n|
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
+n = 302
+while n <= 402
+  Blog.create(
+    title: "あああ",
+    content: "あああ",
+    user_id: n
+  )
+  n = n + 1
+end
